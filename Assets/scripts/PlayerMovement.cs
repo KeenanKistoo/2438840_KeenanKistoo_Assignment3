8764756fe5d9ba7f6e66cac9b2cbaec9cd4b54ac
KeenanKistoo <kistook19@gmail.com>
2023-05-22 18:36:58 +0200

    Movement 50% complete
    Initially, I had an issue where the player would not be moving/looking at the correct angle. I added an offset initially but this still did not work.
    This time I kept my animations on a separate game object.

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Variables:")]
+    public float moveSpeed = 5f;
+    Vector2 moveDirection;
+    Vector2 mousePos;
+
+    [Header("Player Componenets:")]
+    private Rigidbody2D rb;
+
+    [Header("Camera:")]
+    public Camera cam;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical");
+
+        mousePos =  cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+    }
+}
