a27b9f35dab3e5a191afffbdde7b7564b01d68c3
KeenanKistoo <kistook19@gmail.com>
2023-05-22 14:14:01 +0200

    Player Animation Complete

--- Assets/scripts/SelectedCharacter.cs
+++ Assets/scripts/SelectedCharacter.cs
@@ -5,15 +5,9 @@ using UnityEngine;
 public class SelectedCharacter : MonoBehaviour
 {
     public PlayerProfile profile;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        profile = null;
     }
 }
