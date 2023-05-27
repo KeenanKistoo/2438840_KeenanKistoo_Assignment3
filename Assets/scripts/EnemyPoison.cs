7efabac93615f3d8ee6b6e24095fe9b9f4bda019
KeenanKistoo <kistook19@gmail.com>
2023-05-27 16:37:48 +0200

    Basic Enemy Movement/Mechanics Complete

--- Assets/scripts/EnemyPoison.cs
+++ Assets/scripts/EnemyPoison.cs
@@ -6,18 +6,18 @@ public class EnemyPoison : MonoBehaviour
 {
     [Header("Enemy Componenets:")]
     public GameObject enemy;
-    private CircleCollider2D enCollider;
+    
     
 
     [Header("Variables:")]
     private float minRad;
     private float maxRad;
     public bool inc;
-    public bool incObj;
+    
     
     private void Start()
     {
-        enCollider =  enemy.GetComponent<CircleCollider2D>();
+        
         minRad = 0.5f;
         maxRad = 1.5f;
         inc = true;
@@ -25,8 +25,8 @@ public class EnemyPoison : MonoBehaviour
 
     private void Update()
     {
-        //RadiusCheck();
-       ScaleCheck();
+        RadiusCheck();
+      
        
         
     }
@@ -34,44 +34,11 @@ public class EnemyPoison : MonoBehaviour
     public void RadiusCheck()
     {
 
-        if (inc){
-            enCollider.radius += 0.25f * Time.deltaTime;
-            
-            if(enCollider.radius >= maxRad) { 
-              inc = false;
-            }
-            else
-            {
-                return;
-            }
-        }else if (!inc) {
-            enCollider.radius -= 0.25f * Time.deltaTime;
-
-            if(enCollider.radius <= minRad){
-                inc = true;
-            } else { 
-                return;
-            }
-        }
+      
+        
 
 
     }
 
-    public void ScaleCheck()
-    {
-        Vector3 radius = enemy.transform.localScale;
-
-        if (incObj) {
-            enemy.transform.localScale = new Vector3(radius.x + (1f * Time.deltaTime), radius.y + (1f * Time.deltaTime), radius.z);
-        }
-    }
-
-    public void Scale()
-    {
-        Vector3 localScale = enemy.transform.localScale;
-
-        float average = (localScale.x + localScale.y) / 2f;
-
-        enCollider.radius = average;
-    }
+    
 }
